7f7bc3a57e32234ea3d3a78a60e195916de6a17e
stepanpigriteus <avetor-sport@yandex.ru>
2025-08-11 22:56:17 +0500

    Task 1.8 completed

---
+++ 1.8/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "log"
+
+func main() {
+	OnBite(10, 1, 200)
+}
+
+func OnBite(position int, change int, num int) {
+
+	if position < 0 || position > 63 {
+		log.Println("incorrect position")
+		return
+	}
+
+	switch change {
+	case 0:
+		num = num &^ (1 << position)
+		log.Println(num)
+	case 1:
+		num = num | (1 << position)
+		log.Println(num)
+	default:
+		log.Println("incorrect variant")
+	}
+
+}
